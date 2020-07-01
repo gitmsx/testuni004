92d32e92b62ceebff71f73efd3098e1b72087bb2
gitmsx <47451157+gitmsx@users.noreply.github.com>
2020-07-01 18:24:44 +0300

    autoCommit 20200701.182444

--- Assets/scripts/sc003.cs
+++ Assets/scripts/sc003.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class sc003 : MonoBehaviour
 {
@@ -9,52 +10,74 @@ public class sc003 : MonoBehaviour
     public Rigidbody rb;
     public float speed = 1.0f;
     public float rotationSpeed = 1.0f;
-
-
-
-    public float TimeRewind;
-
     public KeyCode rewindButton;
+    public float TimeRewind;
+    public Slider slider;
 
+    
+
+    
     List<Vector3> positionList;
-    bool IsRewind;
+    bool IsRewind = false;
+
+
+
     void Start()
     {
+  
         positionList = new List<Vector3>();
+        slider.maxValue = TimeRewind / Time.fixedDeltaTime;
 
     }
     private void Awake()
     {
+       
         rb = GetComponent<Rigidbody>();
+
     }
     // Update is called once per frame
 
     private void FixedUpdate()
     {
 
-        TimeRewind = 7;
-        while (positionList.Count >= TimeRewind * (1 / 0.02 ))
-        {
-            positionList.RemoveAt(0);
-        }
+        TimeRewind = 5;
+
+   
+
 
         if (IsRewind)
         {
-            int LastPosition = positionList.Count - 1;
-
-            if (LastPosition+1 >0) 
-            { 
-            transform.position = positionList[LastPosition];
+            
+            if (positionList.Count > 0)
+            {
+                int LastPosition = positionList.Count - 1;
+                transform.position = positionList[LastPosition];
                 positionList.RemoveAt(LastPosition);
+                slider.value = positionList.Count;
+                rb.isKinematic = true;
 
             }
+           
+            //if (rb.isKinematic == true)
+            //{
+            //    rb.isKinematic = false;
+            //}
+
         }
 
         else
         {
+
+            while (positionList.Count >= TimeRewind / Time.fixedDeltaTime)
+            {
+                positionList.RemoveAt(0);
+            }
             positionList.Add(transform.position);
-            
+            slider.value = positionList.Count;
+            rb.isKinematic = false;
+
         }
+     
 
     }
 
@@ -63,7 +86,7 @@ public class sc003 : MonoBehaviour
 
 
         float v1 = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        float h1 = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime /10;
+        float h1 = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime / 10;
 
         if (Input.GetKeyDown(KeyCode.F))
         {
@@ -73,9 +96,9 @@ public class sc003 : MonoBehaviour
         {
             IsRewind = false;
         }
-
-        transform.Translate(new Vector3( h1, 0, v1));
-
+       
+        transform.Translate(new Vector3(h1, 0, v1));
+        
         //rb.AddRelativeForce(rotation, 0f, translation);
 
 
